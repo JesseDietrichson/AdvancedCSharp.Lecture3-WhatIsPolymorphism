04d2b5351e92293336fd5dcab3f7494afd9129d1
JesseDietrichson <jesse.dietrichson@xamarin.com>
2016-12-01 18:36:21 -0500

    Add project files.

---
+++ WhatIsPolymorphism/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsPolymorphism
+{
+    class Employee : Person
+    {
+        public string ID { get; set; }
+
+        public void IntroduceSelf()
+        {
+            Console.WriteLine($"Hello my name is {Name} and my id is {ID}");
+        }
+    }
+}
---
+++ WhatIsPolymorphism/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsPolymorphism
+{
+    class Program
+    {
+        //Lecture 3: What is Polymorphism?
+        static void Main(string[] args)
+        {
+            List<Person> emps = new List<Person>();
+
+            Student s = new Student();
+            s.Name = "Jesse";
+            s.Age = 15;
+            s.GradeLevel = 8;
+
+            Employee e = new Employee();
+            e.Name = "Bob";
+            e.Age = 50;
+            e.ID = "234234jdfjsdf";
+
+            emps.Add(s);
+            emps.Add(e);
+
+            foreach (Person p in emps)
+            {
+                if (p is Student)
+                {
+                    Student temp = p as Student;
+                    Console.WriteLine(temp.GradeLevel);
+                }
+                else if (p is Employee)
+                {
+                    Employee temp = p as Employee;
+                    Console.WriteLine(temp.ID);
+                }
+            }
+
+        }
+    }
+}
